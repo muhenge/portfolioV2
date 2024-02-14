f042cd3c79a84bd308687d55eb044544f9a09075
NGENZI Herve <48619051+muhenge@users.noreply.github.com>
2024-02-14 16:55:19 +0200

    Change font intro

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -6,16 +6,15 @@ function Intro() {
       <div className="row m-lg-5 my-lg-5">
         <div className="col-12 col-sm col-md-12 text-lg-center border p-5 shadow">
           <code class="text-dark">
-            <h4 className="border p-2 bg-dark text-light editable" contenteditable="true">console.log("Hello World");_</h4><br/>
+            <h4 className="border p-2 bg-dark text-light editable" contenteditable="true">console.log("Hello There");_</h4><br/>
             <span className="display-5">My name is NGENZI</span>
-            <h3 className="display"> I specialize in web and software development</h3>
-            <span className="">Back-end</span> | <span className="text-center text secondary">Front-end</span> | <span className="text-center text secondary">Full-stack</span>
+            <p className=""> I am a web and a software development</p>
           </code>
           <div/>
         </div>
         <div className="my-5 mt-5">
-          <h5 className="text-center">
-Experienced in both back-end and front-end technologies, along with full-stack solutions, I leverage modern technologies to assist you in bringing your next project to life.          </h5>
+          <p className="text-center">
+Experienced in both back-end and front-end technologies, along with full-stack solutions, I leverage modern technologies to assist you in bringing your next project to life.</p>
         </div>
       </div>
     </div>
